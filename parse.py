02308e3091fc0099ceec6724de592e44035b7bf5
brian112358 <brian112358@gmail.com>
2017-02-15 22:01:56 -0800

    Added code to parse JSON file into NetworkX graph.

---
+++ parse.py
@@ -0,0 +1,20 @@
+import networkx as nx
+from networkx.readwrite import json_graph
+import json
+import os
+
+# Gets the game conditions (num_players, num_seeds, unique_id) from a graph's filename
+def get_game_from_filename(filename):
+    # Filename should be formatted 'num_players.num_seeds.unique_id'
+    parts = os.path.basename(filename).split('.')
+    assert(len(parts) == 3)
+    num_players = int(parts[0])
+    num_seeds = int(parts[1])
+    unique_id = int(parts[2])
+    return num_players, num_seeds, unique_id
+
+# Parses a JSON-formatted graph with the given filename.
+def parse_graph(filename):
+    with open(filename) as infile:
+        js_graph = json.load(infile)
+    return nx.from_dict_of_lists(js_graph)
