38f0ddc9ad35fc4fa3be7303e389d92e10beb5ad
Francescco Macagno <fjmacagno1@yahoo.com>
2017-02-15 22:43:52 -0800

    fixed name usage

--- src/randomSeeds.py
+++ src/randomSeeds.py
@@ -1,13 +1,13 @@
 import sys
 import networkx as nx
-import random
+import random as r
 import parse
 
 if __name__ == '__main__':
     fl = sys.argv[1]
 
-    num_players, num_seeds, unique_id = get_game_info_from_filename(fl)
-    g = parse_graph(fl)
+    num_players, num_seeds, unique_id = parse.get_game_info_from_filename(fl)
+    g = parse.parse_graph(fl)
 
     nodes = nodes(g)
     for i in range(50):
