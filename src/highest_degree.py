bac1e916f940955f5f8560f9fe451892a42065f8
brian112358 <brian112358@gmail.com>
2017-02-15 23:12:32 -0800

    Added strategy to choose highest-degree nodes.

---
+++ src/highest_degree.py
@@ -0,0 +1,10 @@
+import sys
+import networkx as nx
+import heapq
+
+import parse
+
+def choose_nodes_from_graph(graph, num_players, num_seeds):
+    degrees = nx.degree(g)
+    # Just choose the num_seeds highest-degree nodes
+    return heapq.nlargest(num_seeds, degrees.__get_item__)
