c400d0d3cde7918e7fa0634b4dfe60cc4e9468eb
Francescco Macagno <fjmacagno1@yahoo.com>
2017-02-15 22:20:55 -0800

    Implemented simple random seed generator

--- src/random/random.py
+++ src/random/random.py
@@ -1,10 +1,15 @@
 import sys
 import networkx as nx
-from file import *
+import random as r
+from .. import parse
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     fl = sys.argv[1]
 
-    g = loadGraph(fl)
+    num_players, num_seeds, unique_id = get_game_info_from_filename(fl)
+    g = parse_graph(fl)
 
-    
+    nodes = nodes(g)
+    for i in range(50):
+        for x in range(num_seeds):
+            print "%" % nodes[randInt()]
